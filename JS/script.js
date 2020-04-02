613811bffbe9f4c42175ad552c2f676a43a91baf
Oleksandr <OleksandrDeveloper123@gmail.com>
2020-04-02 20:31:55 +0300

    js

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,19 @@
-alert("Hello world")
+// let str = "hello world";
+// alert(str);
+// let a = 1;
+// let b = 1;
+// let c = a + b;
+// console.log(c);
+// let i = 7%2;
+// console.log(i);
+let i = 3, b = '3';
+// ++i;
+// console.log(i**2);
+// console.log(i);
+if(i === b){
+    console.log("однакові")
+} else{
+    console.log("неоднакові")
+}
+let s = (i === b)? true: false;
+console.log(s)
